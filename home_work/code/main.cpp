090b8119019ecafcf5d8f99ced99e778bfe31b18
mshsvnv <mshsvnv@mail.ru>
2023-11-26 13:46:21 +0300

    hope last fixes in hw(

--- home_work/code/main.cpp
+++ home_work/code/main.cpp
@@ -10,47 +10,46 @@ int main() {
 
 srand(time(0));
 
-int size = 4;                                             //(1)
-arrayT arr{4, 3, 2, 1};                                         //(2)
+int size = 4;                                                //(1)
+arrayT arr(size);                                            //(2)
 
-// for (int i = 0; i < size; ++i)                                      
-//     arr[i] = rand() % 100 - 50;                                     
+for (int i = 0; i < size; ++i)                                      
+    arr[i] = rand() % 100 - 50;                                     
 
-// cout << "До: ";
-// for (auto elem : arr)                                               
-//     cout << elem << " ";                                            
-// cout << endl;
+cout << "До: ";
+for (auto elem : arr)                                               
+    cout << elem << " ";                                            
+cout << endl;
 
-for (int step = 1; step < size; step = step * 2){         //(3)
+for (int step = 1; step < size; step = step * 2){            //(3)
 
-    for (int left = 0; left < size - 1; left += 2 * step){//(4)
+    for (int left = 0; left < size - 1; left += 2 * step){   //(4)
 
-        int mid = min(left + step - 1, size - 1),         //(5)
-            right = min(left + 2 * step - 1, size - 1);   //(6)
+        int mid = min(left + step - 1, size - 1),            //(5)
+            right = min(left + 2 * step - 1, size - 1);      //(6)
 
-        int i = left,                                     //(7)
-            j = mid + 1,                                  //(8)
-            pos = 0,                                      //(9)
-            len = right - left + 1;                       //(10)
+        int i = left,                                        //(7)
+            j = mid + 1,                                     //(8)
+            pos = 0,                                         //(9)
+            len = right - left + 1;                          //(10)
 
-        arrayT tmp(len);                                  //(11)
+        arrayT tmp(len);                                     //(11)
 
-        while (i <= mid and j <= right) {                  //(12)
+        while (i <= mid || j <= right) {                     //(12)
 
-            if (arr[i] < arr[j])                          //(13)
-                tmp[pos++] = arr[i++];                    //(14)
-            else                                          
-                tmp[pos++] = arr[j++];                    //(15)
+            if (i <= mid && (j > right || arr[i] < arr[j])) {//(13)
+                tmp[pos] = arr[i];                           //(14)
+                i++;                                         //(15)
+            } 
+            else {
+                tmp[pos] = arr[j];                           //(16) 
+                j++;                                         //(17) 
+            }
+            pos++;                                           //(18) 
         }
 
-        while (i <= mid)                                  //(16)    
-            tmp[pos++] = arr[i++];                        //(17)
-
-        while (j <= right)                                //(18)   
-            tmp[pos++] = arr[j++];                        //(19)
-
-        for (int i = 0; i < len; ++i)                     //(20)
-            arr[i + left] = tmp[i];                       //(21)
+        for (int k = 0; k < len; ++k)                        //(19)
+            arr[k + left] = tmp[k];                          //(20)
     }
 }
 
